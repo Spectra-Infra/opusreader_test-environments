bd73671a2b42c66659f19ed99866238e7c325b87
Philipp Baumann <philipp.baumann@bfh.ch>
2022-01-20 20:04:15 +0100

    Description, basic templating

---
+++ R/switchr_gen_envs/_switchr-gen-env-commit.R
@@ -0,0 +1,11 @@
+################################################################################
+## Scope: Autogenerate scripts that produce reproducible environments
+##   with package cohorts:
+###  - isolate single opusreader github commits into switchr manifests
+##   - create new project-based switchr library
+##   - seed switchr library with defined package manifest (commit-based)
+##   - load minimal set of libraries for additional inspection (checksums etc.)
+## Sources:
+## Outputs: intended side effects of sourcing those autogenerate scripts:
+##   - writes scripts: `./R/switchr_test_envs/
+################################################################################
